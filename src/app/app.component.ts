225f7910f3ef2d8459cd08a63f1a358b445b36a2
ghoshsuman845 <ghoshsuman845@yahoo.com>
2019-08-23 09:53:17 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,92 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prime-pro';
+  users: User[];
+  user:any;
+  newUser:boolean=false;
+  displayDialog:boolean=false;
+
+  cols: any[];
+  profileForm = this.fb.group({
+    id: ['', Validators.required],
+    firstName: ['', Validators.required],
+    email: ['', Validators.required],
+    city:['', Validators.required],
+    state: ['', Validators.required],
+    zip:['', Validators.required]
+  });
+
+  // get aliases() {
+  //   return this.profileForm.get('aliases') as FormArray;
+  // }
+
+  ngOnInit() {
+    this.users = [
+      { id: '', name: '', email:'', city:'' , state:"", zipcode:""}     
+
+  ];
+    this.cols = [
+        { field: 'id', header: 'Id' },
+        { field: 'firstName', header: 'First Name' },
+        { field: 'email', header: 'Email' },
+        { field: 'city', header: 'City' },
+        { field: 'state', header: 'State' },
+        { field: 'zip', header: 'Zip Code' },
+    ];
+}
+
+  constructor(private fb: FormBuilder) { }
+
+
+ 
+
+  // addAlias() {
+  //   this.aliases.push(this.fb.control(''));
+  // }
+
+  showDialogToAdd() {
+      this.newUser = true;
+      this.user = {};
+      this.displayDialog = !this.displayDialog;
+      console.log(this.displayDialog)
+  }
+
+  onSubmit(value) {
+    console.log(value);
+    this.user = value;
+    let users = this.users;
+    users.push(this.user);
+   
+
+    
+    this.users = users
+    this.displayDialog = false;
+    this.clearForm();
+    console.log(this.users)
+  }
+  clearForm() {
+
+    this.profileForm.reset({
+          'id': '',
+          'firstname': '',
+          'email': '',
+         
+          'city': '',
+          'state': '',
+         
+          'zip': ''
+         });
+    }
+  
+  
+}
+export interface User {
+id,
+name,
+email,
+city,
+state,
+zipcode,
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TableModule} from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -10,6 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    TableModule,
+   
     AppRoutingModule
   ],
   providers: [],
